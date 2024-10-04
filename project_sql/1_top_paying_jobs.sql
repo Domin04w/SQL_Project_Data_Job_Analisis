825c00f5cfef4a5ed21dc21d6799220844be9c8f
Domin04w <domin04w@gmail.com>
2024-10-04 20:55:43 +0200

    querry_1

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,17 @@
+SELECT 
+    job_id,
+    name AS company_name,
+    job_title,
+    job_location,
+    salary_year_avg,
+    job_posted_date
+FROM job_postings_fact
+LEFT JOIN company_dim 
+ON job_postings_fact.company_id = company_dim.company_id
+
+WHERE 
+    job_work_from_home = 'True' AND 
+    salary_year_avg IS NOT NULL AND 
+    job_title_short = 'Data Analyst'
+ORDER BY salary_year_avg DESC
+LIMIT 10
